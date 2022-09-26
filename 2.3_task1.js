39484da6830e0913794461bd27504cd979435340
Francisco Abimael Oro Estrada <faoe2003@gmail.com>
2022-09-26 18:38:58 -0500

    Delete 2.3_task1.js

--- 2.3_task1.js
+++
@@ -1,9 +0,0 @@
-﻿//Task 1
-let ticket = {
-    from : "Muzquiz",
-    to : "Copilco",
-    price : 5
-};
-console.log(`Ticket from: ${ticket.from}`);
-console.log(`Ticket to: ${ticket.to}`);
-console.log(`Ticket price: ${ticket.price}`);
