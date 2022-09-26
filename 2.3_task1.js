83569613cabf406426facad57fcc15263fcef804
Francisco Abimael Oro Estrada <faoe2003@gmail.com>
2022-09-26 18:38:12 -0500

    Add files via upload

---
+++ 2.3_task1.js
@@ -0,0 +1,9 @@
+﻿//Task 1
+let ticket = {
+    from : "Muzquiz",
+    to : "Copilco",
+    price : 5
+};
+console.log(`Ticket from: ${ticket.from}`);
+console.log(`Ticket to: ${ticket.to}`);
+console.log(`Ticket price: ${ticket.price}`);
