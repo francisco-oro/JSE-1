d47b02cadec98e918a9e4318b2413a91c481f8ca
Francisco Abimael Oro Estrada <faoe2003@gmail.com>
2022-09-28 14:35:12 -0500

    Add files via upload

---
+++ module3_section3/3.3_task1.js
@@ -0,0 +1,8 @@
+﻿let width, height, length, volume;
+
+width = prompt("Input width");
+height = prompt("Input height");
+length = prompt("Input length");
+
+volume = width * height * length;
+window.alert("The volume is " + volume);
